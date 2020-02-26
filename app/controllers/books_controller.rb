5a4c26c25db5060a21fff80b48fae07b3a38a20e
izawayuusuke <yuu0502@gmail.com>
2020-02-26 11:18:05 +0000

    [Add] view/show,edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
 
   def update
   	book = Book.find(params[:id])
-  	book.update(book_params)
+  	flash[:notice] = "Book was successfully updated" if book.update(book_params)
   	redirect_to book_path(book.id)
   end
 
